98b294f3f662aa9f240b5e99212caa93f2029950
tejasp999 <tejasp999@github.com>
2018-09-05 21:17:27 -0400

    Core data Fetch and Delete

--- GoalSettingApp/Controller/GoalVC.swift
+++ GoalSettingApp/Controller/GoalVC.swift
@@ -13,11 +13,30 @@ let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class GoalVC: UIViewController {
 
+    var goals : [Goal] = []
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchDataObjects()
+        tableView.reloadData()
+    }
+    
+    func fetchDataObjects(){
+        self.fetch { (complete) in
+            if complete{
+                if goals.count >= 1{
+                    tableView.isHidden = false
+                }
+            }else{
+                tableView.isHidden = true
+            }
+        }
     }
 
     @IBAction func addButtonAction(_ sender: Any) {
@@ -34,14 +53,80 @@ extension GoalVC : UITableViewDataSource, UITableViewDelegate{
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return goals.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let goal = goals[indexPath.row]
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else{
             return UITableViewCell()
         }
-        cell.configureCell(description: "Eat salad thrice a week", type: .shortTerm, progress: "3")
+        cell.configureCell(goal: goal)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.none
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { (rowAction, indexPath) in
+            self.removeGoal(atIndexPath: indexPath)
+            self.fetchDataObjects()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        let addAction = UITableViewRowAction(style: .normal, title: "Add 1") { (rowAction, indexPath) in
+            self.set(atIndexPath: indexPath)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
+        deleteAction.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        addAction.backgroundColor = #colorLiteral(red: 0.9771530032, green: 0.7062081099, blue: 0.1748393774, alpha: 1)
+        return [deleteAction, addAction]
+    }
+}
+
+extension GoalVC{
+    func removeGoal(atIndexPath indexPath: IndexPath){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        managedContext.delete(goals[indexPath.row])
+        do{
+            try managedContext.save()
+        }catch{
+            debugPrint("Could not remove \(error.localizedDescription)")
+        }
+    }
+    
+    func fetch(completion : (_ complete: Bool) -> ()){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
+        do{
+            goals = try managedContext.fetch(fetchRequest)
+            print("The fetch was successful")
+            completion(true)
+        } catch{
+            debugPrint("Could not fetch \(error.localizedDescription)")
+            completion(false)
+        }
+    }
+    
+    func set(atIndexPath indexPath: IndexPath){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let chosenGoal = goals[indexPath.row]
+        if chosenGoal.goalProgress < chosenGoal.goalCompletionValue{
+            chosenGoal.goalProgress = chosenGoal.goalProgress + 1
+        }else {
+            return
+        }
+        do{
+            try managedContext.save()
+        }catch{
+            debugPrint("Could not set progress \(error.localizedDescription)")
+        }
     }
 }
 
--- GoalSettingApp/View/GoalCell.swift
+++ GoalSettingApp/View/GoalCell.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class GoalCell: UITableViewCell {
 
+    @IBOutlet weak var completionView: UIView!
     @IBOutlet weak var goalDescriptionLbl: UILabel!
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
-    func configureCell(description : String, type : GoalType, progress : Int){
-        self.goalDescriptionLbl.text = description
-        self.goalTypeLbl.text = type.rawValue
-        self.goalProgressLbl.text = String(describing: progress)
+    func configureCell(goal : Goal){
+        self.goalDescriptionLbl.text = goal.goalDescription
+        self.goalTypeLbl.text = goal.goalType
+        self.goalProgressLbl.text = String(describing: goal.goalProgress)
+        if goal.goalProgress == goal.goalCompletionValue{
+            completionView.isHidden = false
+        }else{
+            completionView.isHidden = true
+        }
     }
 }
